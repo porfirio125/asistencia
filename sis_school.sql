ff4f8f60b36b5b7c3ed4506564c005617391c662
porfirio287 <132922567+porfirio125@users.noreply.github.com>
2023-11-20 10:27:33 -0500

    Update sis_school.sql

--- sis_school.sql
+++ sis_school.sql
@@ -234,9 +234,7 @@ CREATE TABLE `usuario` (
 --
 
 INSERT INTO `usuario` (`idusuario`, `nombre`, `tipo_documento`, `num_documento`, `direccion`, `telefono`, `email`, `cargo`, `login`, `clave`, `imagen`, `condicion`) VALUES
-(1, 'demo', 'DNI', '72154871', 'Calle los alpes 210', '547821', 'admin@gmail.com', 'Administrador', 'admin', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', '1535417472.jpg', 0),
-(3, 'Prueba', 'DNI', '74725546', 'Acora', '987654321', 'samoporfirio125@gmail.com', 'Administrador', 'prueba', '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92', '1699284352.png', 0),
-(4, 'Wilson Porfirio', 'DNI', '74725546', 'ACORA', '987654321', 'samoporfirio125@gmail.com', 'Administrador', 'Porfirio', '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92', '1700141646.png', 1);
+(1, 'demo', 'DNI', '72154871', 'Calle los alpes 210', '547821', 'admin@gmail.com', 'Administrador', 'admin', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', '1535417472.jpg', 0);
 
 -- --------------------------------------------------------
 
